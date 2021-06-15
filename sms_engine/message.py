dec65e2918c46653bf9d616234d1d213a860fd53
aleks-dimoski <aleksdimoski890@gmail.com>
2021-06-15 06:07:09 +0100

    Began work on message client

--- sms_engine/message.py
+++ sms_engine/message.py
@@ -1,1 +1,34 @@
+import smtplib, ssl
+import json
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
+f = open('EMAIL_ADDRESS.json')
+data = json.load(f)
+sender = data['sender']
+receiver = data['receiver']
+password = data['password']
+f.close()
+
+smtp_server = "smtp.gmail.com"
+port = 587
+context = ssl.create_default_context()
+
+def send_msg(message):
+    try:
+        server = smtplib.SMTP(smtp_server, port)
+        server.starttls(context=context)
+        server.login(sender, password)
+        msg = MIMEText(message, 'plain')
+        msg['From']='Dimoski Database'
+        msg['To']=receiver
+        msg['Subject']=''
+        msg = msg.as_string()
+        server.sendmail(sender,receiver,msg)
+        del msg
+    except Exception as e:
+        print(e)
+    finally:
+        server.quit()
+
+#send_msg("Test of my system - hope it works")
